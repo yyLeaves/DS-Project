82c691608500bed42f796d4879201e689180d5b9
Mike <49346779+Koukotsukan@users.noreply.github.com>
2021-06-04 21:31:54 +0800

    Update E6Friendship

--- project_sociopath/src/com/sociopath/events/E6Friendship.java
+++ project_sociopath/src/com/sociopath/events/E6Friendship.java
@@ -1,29 +1,72 @@
-package com.sociopath.events;
-
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class E6Friendship {
+    private int vertex;
+    private ArrayList<Integer>[] adjacencyList;
 
+    public E6Friendship(int vertices) {
 
+        this.vertex = vertices;
+
+        initAdjList();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initAdjList() {
+        adjacencyList = new ArrayList[vertex];
+
+        for (int i = 0; i < vertex; i++) {
+            adjacencyList[i] = new ArrayList<>();
+        }
+    }
+
+    public void addEdge(int from, int to) {
+        adjacencyList[from].add(to);
+        adjacencyList[to].add(from);
+    }
+
+    public void findPathList(int source, int destination) {
+        boolean[] isVisited = new boolean[vertex];
+        ArrayList<Integer> pathList = new ArrayList<>();
+        pathList.add(source);
+        findAllPaths(source, destination, isVisited, pathList);
+    }
+
+    private void findAllPaths(Integer u, Integer d, boolean[] isVisited, List<Integer> pathList) {
+        if (u.equals(d)) {
+            for(Integer integer:pathList)
+                System.out.print(integer + 1 + " ");
+            System.out.println();
+            return;
+        }
+        isVisited[u] = true;
+        for (Integer i : adjacencyList[u]) {
+            if (!isVisited[i]) {
+                pathList.add(i);
+                findAllPaths(i, d, isVisited, pathList);
+                pathList.remove(i);
+            }
+        }
+        isVisited[u] = false;
+    }
 
     public static void main(String[] args) {
-        Graph<Integer> graph = new Graph<>();
-            Scanner s =new Scanner(System.in);
-            int amount = s.nextInt();
-            E6Friendship test = new E6Friendship();
-            while(amount != 0){
-                int temp1 = s.nextInt();
-                int temp2 = s.nextInt();
-                ArrayList<Integer> tmpArrayList = new ArrayList<>();
-                tmpArrayList.add(temp1);
-                tmpArrayList.add(temp2);
-                System.out.println(temp1 + " " + temp2);
-                graph.addVertex(temp1);
-                graph.addVertex(temp2);
-                graph.addUndirectedEdge(temp1,temp2);
-                graph.printEdges();
-                amount--;
+        Scanner x = new Scanner(System.in);
+        System.out.println("Input:");
+        int amountOfVertices = x.nextInt();
+        E6Friendship g = new E6Friendship(amountOfVertices);
+        for (int i=0; i<amountOfVertices; i++){
+            g.addEdge(x.nextInt()-1, x.nextInt()-1);
+        }
+        System.out.println("Output:");
+        for (int i=0; i<amountOfVertices;i++){
+            for(int n=0; n<amountOfVertices; n++){
+                if(i<n){
+                    g.findPathList(i, n);
+                }
             }
+        }
     }
 }
