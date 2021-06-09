09a686bf16111c11955ab6437d6342ca2d7cabc9
yyLeaves <luckyyeyang@gmail.com>
2021-06-09 08:18:50 +0800

    change the use of dive rate

--- project_sociopath/src/com/sociopath/ui/GraphInterface.java
+++ project_sociopath/src/com/sociopath/ui/GraphInterface.java
@@ -6,10 +6,6 @@ import com.sociopath.util.ScreenUtils;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 
