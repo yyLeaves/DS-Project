b9c23a0a0522bfc6d687ab1febe92b4a95235196
Yeyang <76979429+yyLeaves@users.noreply.github.com>
2021-06-17 09:28:29 +0800

    Update E5meetCrush.java

--- project_sociopath/src/com/sociopath/events/E5meetCrush.java
+++ project_sociopath/src/com/sociopath/events/E5meetCrush.java
@@ -1,168 +1,228 @@
-package com.sociopath.ui;
+package com.sociopath.events;
 
-import com.sociopath.component.*;
-import com.sociopath.events.E0Init;
-import com.sociopath.events.Student;
-import com.sociopath.util.PathUtils;
-import com.sociopath.util.ScreenUtils;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.*;
 
-/**
- * Description: events interface
- *
- */
-public class EventsInterface extends JFrame {
+public class E5meetCrush {
 
-    final int WIDTH = 1000;
-    final int HEIGHT = 600;
+    private int currentStudentId;
+    private int crush;
+    private int spreader;
+    private boolean first = true;
+    private boolean con;
 
-    private ArrayList<Student> students = null;
 
-    public EventsInterface(ArrayList<Student>students, Integer id) {
-        this.students=students;
-        Student.setCurrentStudentId(id);
+    int convince = 0, KnowSize = 1;
+    private int Vertices;   // No. of vertices
+    private LinkedList<Integer> adj_list[]; //Adjacency Lists
+    LinkedList<Integer> queue = new LinkedList<Integer>();
+    LinkedList<Boolean> visit = new LinkedList<>();
+
+
+    public E5meetCrush(int v, int currentStudentId) {
+        Vertices = v;
+        adj_list = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {         //create adjacency lists
+            adj_list[i] = new LinkedList();
+            visit.add(i, Boolean.FALSE);
+        }
     }
 
-    public void initStudents() {
-        students = E0Init.init();
-        System.out.println("== Initializing... ==");
-        Student.printStudents(students);
-        System.out.println();
-    }
-    public static void main(String[] args) throws IOException {
-        new EventsInterface(E0Init.init(),Student.getCurrentStudentId()).init();
+
+    // add an edge to the graph
+    void addEdge(int v, int w) {
+        adj_list[v].add(w);
+        adj_list[w].add(v);
     }
 
-    public void init() throws IOException {
-        this.setTitle("Hello, Student" + Student.getCurrentStudentId() + "!");
-        setBounds(ScreenUtils.getScreenWidth() / 2 - WIDTH / 2, ScreenUtils.getScreenHeight() / 2 - HEIGHT / 2, WIDTH, HEIGHT);
-        setResizable(false);
-        setIconImage(ImageIO.read(new File(PathUtils.getPath("event_icon_image"))));
 
-        // set menu bar
-        JMenuBar menuBar = new JMenuBar();
-        JMenu settingMenu = new JMenu("Settings");
-        JMenuItem switchItem = new JMenuItem("Switch");
-        JMenuItem exitItem = new JMenuItem("Exit");
-        JMenuItem initItem = new JMenuItem("Initialize");
-        switchItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    new MainInterface(students).init();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                dispose();
-            }
-        });
-        exitItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("== Bye Bye ==\nヾ(￣▽￣)Bye~Bye~");
-                System.exit(0);
-            }
-        });
-        initItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("== Initialize ==\n You will be logged out and the history will be cleared permanently. The data will be initialized. Please Confirm!\n");
+    void BFS(int currentStudentId) {
+
+//        System.out.println("c:"+currentStudentId);
+        do{
+            crush =(int) (Math.random()*10+1);
+        } while (crush==currentStudentId);
+
+/*        this.crush = crush(currentStudentId);
+        this.spreader = spreader(crush,currentStudentId);*/
+
+        do{
+            spreader = (int) (Math.random()*10+1);
+        } while (spreader==crush||spreader==currentStudentId);
+
+        System.out.println("Your have a crush on Student " + crush);
 
 
-                int initOrNot = JOptionPane.showConfirmDialog(null, "The history will be permanently cleared, are you sure you want to init?",
-                        "Initialize Warning", JOptionPane.YES_NO_OPTION);
-                if(initOrNot==0) {
-                    Student.resetCount();
-                    students = E0Init.init();
-                    System.out.println("== Initializing ==");
-                    Student.printStudents(students);
-                    System.out.println();
-                    try {
-                        new MainInterface(students).init();
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
+        int day = 1;
+        System.out.println("Stanger " + spreader + " starts to spread rumors about you\n");
+        // BFS queue
+
+        // current node = visited, insert into queue
+        visit.set(spreader, Boolean.TRUE);
+        queue.add(spreader);
+
+
+        while (queue.size() != 0) {
+
+            LinkedList<Integer> nextQueue = new LinkedList<Integer>();
+            boolean dayPrinted = false;
+            con = true;
+
+            // traversed ALL the nodes in this layer
+            // and put them into the nextLayer queue
+            while (queue.size() != 0) {
+                // deque an entry from queue and process it
+                spreader = queue.poll();
+
+
+                // get all adjacent nodes of current node and process
+                Iterator<Integer> i = adj_list[spreader].listIterator();
+                while (i.hasNext()) {
+                    int n = i.next();
+
+                    if (visit.get(n).booleanValue() == Boolean.FALSE) {
+                        if (!dayPrinted) {
+                            System.out.println("**** Day " + day + " ****");
+                            dayPrinted = true;
+                        }
+                        visit.set(n, Boolean.TRUE);
+                        nextQueue.add(n);
+                        KnowSize++;
+                        //System.out.print("spreaded to student: ");
+                        System.out.print(n + " ");
+
                     }
-                    dispose();
                 }
-            }
-        });
-
-        settingMenu.add(switchItem);
-        settingMenu.add(exitItem);
-        settingMenu.add(initItem);
-
-        menuBar.add(settingMenu);
-
-//        add(menuBar);
-        setJMenuBar(menuBar);
-
-        // split pane
-        JSplitPane pane = new JSplitPane();
-        pane.setContinuousLayout(true);
-        pane.setDividerLocation(150);
-        pane.setDividerSize(5);
-
-
-        // left panel
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Events");
-        DefaultMutableTreeNode stats = new DefaultMutableTreeNode("Statistics");
-        // teach, chit chat, friendship
-        DefaultMutableTreeNode event1 = new DefaultMutableTreeNode("Friends");
-//        DefaultMutableTreeNode event2 = new DefaultMutableTreeNode("???");
-        DefaultMutableTreeNode libEvent = new DefaultMutableTreeNode("Library");
-        root.add(stats);
-        root.add(stats);
-        root.add(event1);
-        root.add(libEvent);
-
-        JTree tree = new JTree(root);
-        tree.setSelectionRow(0);
-        tree.addTreeSelectionListener(new TreeSelectionListener() {
-            @Override
-            public void valueChanged(TreeSelectionEvent e) {
-                Object lastPathComponent = e.getNewLeadSelectionPath().getLastPathComponent();
-                if (root.equals(lastPathComponent)) {
-                    pane.setRightComponent(new PanelIntroduction());
-                    pane.setDividerLocation(150);
-//                    pane.setRightComponent(new EventPanel());
-                } else if (stats.equals(lastPathComponent)) {
-                    // TODO: 2021/5/27 Debug
-                    pane.setRightComponent(new BoxStudentInfo(20,students));
-                    pane.setDividerLocation(150);
-                } else if (event1.equals(lastPathComponent)) {
-                    pane.setRightComponent(new BoxFriend(students,Student.getCurrentStudentId()));
-                    pane.setDividerLocation(150);
-                } /*else if (event2.equals(lastPathComponent)) {
-                    pane.setRightComponent(new JLabel("wt"));
-                    pane.setDividerLocation(150);
-                }*/ else if (libEvent.equals(lastPathComponent)) {
-                    pane.setRightComponent(new BoxLib());
-                    pane.setDividerLocation(150);
+                if (nextQueue.contains(currentStudentId)) {      //YOU
+                    System.out.println("\nRumors spread to your ear,you stop spreading");
+                    int y = nextQueue.indexOf(currentStudentId);        //remove object
+                    nextQueue.remove(y);
                 }
 
+                if (nextQueue.contains(crush)) {
+                    con = false;
+                    queue.clear();
+                    System.out.println("\nOh no, your crush(" + crush + ")" + " knows!\n");
+                }
             }
-        });
-        pane.setLeftComponent(tree);
-        pane.getLeftComponent().setBackground(new Color(0xB5CCEE));
+            if (dayPrinted && con) {
+                queue = nextQueue;
+                convinceOne();
+                day++;
+            }
+        }
+        if (con != false) {
+            System.out.println("Rumors will not reach to your crush, GOODLUCK!");
+        }
+        end();
+    }
 
-        pane.setRightComponent(new PanelIntroduction());
+/*    public int crush(int currentStudentId) {
+        Random r = new Random();
+        int crush = r.nextInt(10) + 1;
+
+        if (crush == currentStudentId) {
+            return crush(currentStudentId);
+        } else {
+            return crush;
+        }
+    }*/
+
+/*    public int spreader(int crush, int currentStudentId) {
+        *//*LinkedList<Integer> keys = new LinkedList<Integer>();
+        keys.add(crush);
+
+        Random r=new Random();
+        int spreader= r.nextInt(10)+1;
+        if (!keys.contains(spreader)) {
+            keys.add(spreader);
+            return spreader;
+        } else {
+            return spreader(crush);
+        }*//*
+        int i = (int) (Math.random() * 10 + 1);
+        if (i != crush && i != currentStudentId) {
+            return i;
+        } else {
+            return spreader(crush,currentStudentId);
+        }
+    }*/
 
 
-        add(pane);
+    public void convinceOne() {
 
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        int input=0;
+/*        if (first) {
+            System.out.println("You can't convince anyone in the first day!\n");
+            first = false;
+        } else {*/
+            System.out.println("\nWho do you want to convience? Press 0 if none");
+            System.out.print("Convince: ");
+/*        Scanner s=new Scanner(System.in);
+        int input= s.nextInt();*/
+
+
+            String str = JOptionPane.showInputDialog(null, "who?", "convince", JOptionPane.PLAIN_MESSAGE);
+            if (str == null) {
+                input = 0;
+            } else {
+                input = Integer.parseInt(str);
+            }
+
+
+        if (input == 0) {
+            System.out.println("You did not convience anyone\n");
+        } else if (!queue.contains(input)) {
+            convinceOne();
+        } else {
+            convince++;
+
+            // get all adjacent nodes of current node and process
+            Iterator<Integer> i = adj_list[input].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (visit.get(n).booleanValue() == Boolean.FALSE) {
+                    visit.set(input, Boolean.TRUE);
+                }
+            }
+            int y = queue.indexOf(input);        //remove object
+            queue.remove(y);
+
+            System.out.println("This student stops spreading to his friends\n");
+        }
+//        }
+    }
+
+    public void end() {
+        System.out.println(":: " + KnowSize + " person knows about your rumors");
+        System.out.println(":: " + convince + " person has been convinced");
+    }
+
+    public void meet(int currentStudentId) {
+//        this.currentStudentId = currentStudentId;
+//        System.out.println(currentStudentId);
+        E5meetCrush g = new E5meetCrush(11,currentStudentId);
+        //add edges to the graph
+        g.addEdge(1, 2);
+        g.addEdge(1, 7);
+        g.addEdge(2, 3);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+        g.addEdge(4, 8);
+        g.addEdge(4, 10);
+        g.addEdge(9, 10);
+
+        //print BFS sequence
+        g.BFS(currentStudentId);
+    }
+
+    public static void main(String args[]) {
+        //vertices
+
+        E5meetCrush e5meetCrush = new E5meetCrush(11,5);
+        e5meetCrush.meet(5);
     }
 
 }
